24e40e12443951985568c55aed288ae3fa4c6d9a
Mateus Teixeira <mateus.teixeira2394@gmail.com>
2023-03-17 14:01:29 -0300

    Adjusted the carousel to the tablet width

--- javascript/my-swipe.js
+++ javascript/my-swipe.js
@@ -2,6 +2,13 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     slidesPerView: 3,
     spaceBetween: 10,
+    
+    breakpoints:{
+        820:{
+            slidesPerView: 3,
+            spaceBetween: 70,
+        }
+    },
 
     // If we need pagination
     pagination: {
